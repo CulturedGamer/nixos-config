e42709429038bcab7324a495dd26ff41ffdfc642
CulturedGamer <dony357@outlook.com>
2023-07-09 13:09:50 -0700

    activated home manager

--- flake.nix
+++ flake.nix
@@ -15,11 +15,11 @@
                 system = "x86_64-linux";
                 modules = [
                     ./configuration.nix
-                    #home-manager.nixosModules.home-manager {
-                    #    home-manager.useGlobalPkgs = true;
-                    #    home-manager.useUserPackages = true;
-                    #    home-manager.users.donny = import ./home;
-                    #}
+                    home-manager.nixosModules.home-manager {
+                       home-manager.useGlobalPkgs = true;
+                       home-manager.useUserPackages = true;
+                       home-manager.users.donny = import ./home;
+                    }
                 ];
             };
         };
