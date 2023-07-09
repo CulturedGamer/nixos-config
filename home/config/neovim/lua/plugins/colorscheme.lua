583973e8f8e5d38056310cf4124dfd853fccea62
CulturedGamer <dony357@outlook.com>
2023-07-09 15:16:20 -0700

    changed colorscheme

--- home/config/neovim/lua/plugins/colorscheme.lua
+++ home/config/neovim/lua/plugins/colorscheme.lua
@@ -3,6 +3,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme 'base16-ayu-dark'
+        vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
     end,
 }
