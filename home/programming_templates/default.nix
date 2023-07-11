89ca0ded5962a6c6cdb4192e853a2a5af569b750
CulturedGamer <dony357@outlook.com>
2023-07-11 15:40:23 -0700

    fixed programming templates path

--- home/programming_templates/default.nix
+++ home/programming_templates/default.nix
@@ -1,17 +1,17 @@
 { config, pkgs, ... }:
 
 {
-    home.file."~/Templates/c_template" = {
+    home.file."Templates/c_template" = {
         source = ./c_template;
         recursive = true;
     };
 
-    home.file."~/Templates/cpp_template" = {
+    home.file."Templates/cpp_template" = {
         source = ./cpp_template;
         recursive = true;
     };
 
-    home.file."~/Templates/opengl_template" = {
+    home.file."Templates/opengl_template" = {
         source = ./opengl_template;
         recursive = true;
     };
