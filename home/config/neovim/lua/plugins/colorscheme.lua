b4671bf6e3d46b26d748f239ba52a377bdd38b6d
CulturedGamer <dony357@outlook.com>
2023-07-11 18:19:54 -0700

    changed neovim config

--- home/config/neovim/lua/plugins/colorscheme.lua
+++ home/config/neovim/lua/plugins/colorscheme.lua
@@ -1,8 +1,28 @@
 return {
-    'RRethy/nvim-base16',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
-    end,
+    {
+        'RRethy/nvim-base16',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+        end 
+    },
+
+    {
+        'EdenEast/nightfox.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme 'nightfox'
+        end
+    },
+
+    {
+        'Tsuzat/NeoSolarized.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme 'NeoSolarized'
+        end
+    },
 }
