bbe31c31d3a8a313ac846709de6de70c071de538
CulturedGamer <dony357@outlook.com>
2023-07-14 13:53:54 -0700

    changes

--- home/default.nix
+++ home/default.nix
@@ -20,13 +20,6 @@
         executable = true;
     };
 
-    # enable the NUR
-    nixpkgs.config.packageOverrides = pkgs: {
-        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-            inherit pkgs;
-        };
-    };
-
     programs.home-manager.enable = true;
     home.stateVersion = "23.05";
 }
