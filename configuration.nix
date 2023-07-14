ecc53f2c4a8be9a106f1cbad23d18ccea3c88471
CulturedGamer <dony357@outlook.com>
2023-07-14 12:20:18 -0700

    added nur install to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -76,7 +76,14 @@
         extraGroups = [ "networkmanager" "wheel" ];
     };
 
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs.config = {
+        allowUnfree = true;
+        packageOverrides = pkgs: {
+            nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+                inherit pkgs;
+            };
+        };
+    };
 
     programs.dconf.enable = true;
 
