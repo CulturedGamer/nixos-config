622bbbc17d9c156b8cff6cd4a20315a9aa6ff0f4
CulturedGamer <dony357@outlook.com>
2023-07-14 16:57:14 -0700

    changes

--- flake.nix
+++ flake.nix
@@ -16,7 +16,6 @@
                 system = "x86_64-linux";
                 modules = [
                     ./configuration.nix
-                    nur.nixosModules.nur
                     home-manager.nixosModules.home-manager {
                        home-manager.useGlobalPkgs = true;
                        home-manager.useUserPackages = true;
