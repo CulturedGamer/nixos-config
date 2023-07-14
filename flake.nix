b5f4bc1b0e0c7d21352f862d3008142d0b7f25f3
CulturedGamer <dony357@outlook.com>
2023-07-13 18:02:26 -0700

    added the nur

--- flake.nix
+++ flake.nix
@@ -3,18 +3,20 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nur.url = "github:nix-community/NUR";
         home-manager = {
             url = "github:nix-community/home-manager/release-23.05";  
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
-    outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    outputs = inputs@{ self, nixpkgs, nur, home-manager, ... }: {
         nixosConfigurations = {
             nixos = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
                 modules = [
                     ./configuration.nix
+                    nur.nixoxModules.nur
                     home-manager.nixosModules.home-manager {
                        home-manager.useGlobalPkgs = true;
                        home-manager.useUserPackages = true;
