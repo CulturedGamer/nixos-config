766e8821fbea45021486446145287a5e693b323f
CulturedGamer <dony357@outlook.com>
2023-07-16 23:26:15 -0700

    changes

--- home/default.nix
+++ home/default.nix
@@ -3,7 +3,12 @@
 {
     imports = [
         ./configs
-        ./scripts
         ./templates
     ];
+
+    home.file.".local/bin" = {
+        source = ./scripts;
+        recursive = true;
+        executable = true;
+    };
 }
