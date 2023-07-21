b5884d8796a15beb1169d75a6a87a73de4f99e53
CulturedGamer <dony357@outlook.com>
2023-07-21 13:20:16 -0700

    changed qtile config

--- home/configs/display/qtile.nix
+++ home/configs/display/qtile.nix
@@ -11,31 +11,20 @@
             terminal = "alacritty" 
 
             keys = [
-                # A list of available commands that can be bound to keys can be found
-                # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-                # Switch between windows
                 Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
                 Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
                 Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
                 Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
                 Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-                # Move windows between left/right columns or move up/down in current stack.
-                # Moving out of range in Columns layout will create new column.
                 Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
                 Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
                 Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
                 Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-                # Grow windows. If current window is on the edge of screen and direction
-                # will be to screen edge - window would shrink.
                 Key([mod, "control"], "h", lazy.layout.shrink_main(), desc="Grow window to the left"),
                 Key([mod, "control"], "l", lazy.layout.grow_main(), desc="Grow window to the right"),
                 Key([mod, "control"], "j", lazy.layout.shrink(), desc="Grow window down"),
                 Key([mod, "control"], "k", lazy.layout.grow(), desc="Grow window up"),
                 Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-                # Toggle between split and unsplit sides of stack.
-                # Split = all windows displayed
-                # Unsplit = 1 window displayed, like Max layout, but still with
-                # multiple stack panes
                 Key(
                     [mod, "shift"],
                     "Return",
@@ -43,14 +32,12 @@
                     desc="Toggle between split and unsplit sides of stack",
                 ),
                 Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-                # Toggle between different layouts as defined below
                 Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
                 Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
                 Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
                 Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
                 Key([mod, "shift"], "space", lazy.window.toggle_floating(), desc="Toggle floating on focused window"),
                 Key([mod], "m", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
-                # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
             ]
 
             groups = []
@@ -95,23 +82,12 @@
                         border_normal='#7aa2f7'
                     ),
                 layout.Max(),
-                # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-                # Try more layouts by unleashing below layouts.
-                # layout.Stack(num_stacks=2),
-                # layout.Bsp(),
-                # layout.Matrix(),
-                # layout.MonadWide(),
-                # layout.RatioTile(),
-                # layout.Tile(),
-                # layout.TreeTab(),
-                # layout.VerticalTile(),
-                # layout.Zoomy(),
             ]
 
             widget_defaults = dict(
                 font="RobotoMonoNerdFont",
-                background="#1f2329",
-                foreground="#a0a8b7",
+                background="#1E1E1E",
+                foreground="#D4D4D4",
                 fontsize=12,
                 padding=3,
             )
@@ -123,8 +99,8 @@
                         [
                             widget.GroupBox(
                                 padding=0,
-                                highlight_method="text",
-                                highlight_color="#7dcfff",
+                                highlight_method="line",
+                                this_current_screen_border="#0195F7",
                                 hide_unused=True
                             ),
                             widget.WindowName(
@@ -149,7 +125,6 @@
                 ),
             ]
 
-            # Drag floating layouts.
             mouse = [
                 Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
                 Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
@@ -166,7 +141,6 @@
                 border_focus = '#4fa6ed',
                 border_normal = '#4fa6ed',
                 float_rules=[
-                    # Run the utility of `xprop` to see the wm class and name of an X client.
                     *layout.Floating.default_float_rules,
                     Match(wm_class="confirmreset"),  # gitk
                     Match(wm_class="makebranch"),  # gitk
@@ -180,8 +154,6 @@
             focus_on_window_activation = "smart"
             reconfigure_screens = True
 
-            # If things like steam games want to auto-minimize themselves when losing
-            # focus, should we respect this or not?
             auto_minimize = True
 
             # When using the Wayland backend, this can be used to configure input devices.
