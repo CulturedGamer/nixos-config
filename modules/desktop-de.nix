106582a4c91376c4536f688a1b28eb9e9a4f2e6e
CulturedGamer <dony357@outlook.com>
2023-07-23 15:58:38 -0700

    updates

--- modules/desktop-de.nix
+++ modules/desktop-de.nix
@@ -14,24 +14,6 @@
         ../home/configs/terminal/lf.nix
     ];
 
-    time.timeZone = "America/Los_Angeles";
-
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-
-    services.printing.enable = true;
-
     fonts = {
         fonts = with pkgs; [
             material-design-icons
@@ -51,10 +33,6 @@
         };
     };
 
-    networking.hostName = "nixos";
-    # networking.wireless.enable = true;
-    networking.networkmanager.enable = true;
-
     services.xserver.enable = true;
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
@@ -62,26 +40,6 @@
         layout = "us";
         xkbVariant = "";
     };
-
-    sound.enable = true;
-    hardware.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-    };
-
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    
-    users.users.donny = {
-        isNormalUser = true;
-        description = "donny";
-        extraGroups = [ "networkmanager" "wheel" ];
-    };
-
-    nixpkgs.config.allowUnfree = true;
 
     programs.dconf.enable = true;
 
@@ -93,6 +51,4 @@
         wget
 	    xclip
     ];
-
-    system.stateVersion = "23.05";
 }
--- modules/desktop-wm.nix
+++ modules/desktop-wm.nix
@@ -24,24 +24,6 @@
         ./misc/sxhkd.nix
     ];
 
-    time.timeZone = "America/Los_Angeles";
-
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-
-    services.printing.enable = true;
-
     fonts = {
         fonts = with pkgs; [
             material-design-icons
@@ -61,10 +43,6 @@
         };
     };
 
-    networking.hostName = "nixos";
-    # networking.wireless.enable = true;
-    networking.networkmanager.enable = true;
-
     services.xserver.enable = true;
     services.xserver.displayManager.startx.enable = true;
     services.xserver.windowManager.qtile.enable = true;
@@ -72,26 +50,6 @@
         layout = "us";
         xkbVariant = "";
     };
-
-    sound.enable = true;
-    hardware.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-    };
-
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    
-    users.users.donny = {
-        isNormalUser = true;
-        description = "donny";
-        extraGroups = [ "networkmanager" "wheel" ];
-    };
-
-    nixpkgs.config.allowUnfree = true;
 
     programs.dconf.enable = true;
 
@@ -103,6 +61,4 @@
         wget
 	    xclip
     ];
-
-    system.stateVersion = "23.05";
 }
