98f43e8d14d737f74646dfc47cfdbbc213520144
CulturedGamer <dony357@outlook.com>
2023-07-25 16:57:54 -0700

    changes

--- home/configs/imports/plasma-imports.nix
+++ home/configs/imports/plasma-imports.nix
@@ -2,16 +2,16 @@
 
 {
     imports = [
-        ./misc/firefox.nix
+        ../misc/firefox.nix
 
-        ./terminal/alacritty
-        ./terminal/neovim
-        ./terminal/bash.nix
-        ./terminal/fish.nix
-        ./terminal/git.nix
-        ./terminal/starship.nix
-        ./terminal/tmux.nix
-        ./terminal/lf.nix
+        ../terminal/alacritty
+        ../terminal/neovim
+        ../terminal/bash.nix
+        ../terminal/fish.nix
+        ../terminal/git.nix
+        ../terminal/starship.nix
+        ../terminal/tmux.nix
+        ../terminal/lf.nix
     ];
 
     home.packages = with pkgs; [
--- home/configs/imports/qtile-imports.nix
+++ home/configs/imports/qtile-imports.nix
@@ -2,25 +2,25 @@
 
 {
     imports = [
-        ./display/wallpaper
-        ./display/gtk.nix
-        ./display/picom.nix
-        ./display/qtile.nix
-        ./display/xinit.nix
+        ../display/wallpaper
+        ../display/gtk.nix
+        ../display/picom.nix
+        ../display/qtile.nix
+        ../display/xinit.nix
 
-        ./terminal/alacritty
-        ./terminal/neovim
-        ./terminal/bash.nix
-        ./terminal/fish.nix
-        ./terminal/git.nix
-        ./terminal/starship.nix
-        ./terminal/tmux.nix
-        ./terminal/lf.nix
+        ../terminal/alacritty
+        ../terminal/neovim
+        ../terminal/bash.nix
+        ../terminal/fish.nix
+        ../terminal/git.nix
+        ../terminal/starship.nix
+        ../terminal/tmux.nix
+        ../terminal/lf.nix
 
-        ./misc/rofi
-        ./misc/bookmarks.nix
-        ./misc/firefox.nix
-        ./misc/sxhkd.nix
+        ../misc/rofi
+        ../misc/bookmarks.nix
+        ../misc/firefox.nix
+        ../misc/sxhkd.nix
     ];
 
     home.packages = with pkgs; [
