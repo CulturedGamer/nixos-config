9fc4f4cd42cfb03d4b8e1ddc2b6535ead37cc81a
CulturedGamer <dony357@outlook.com>
2023-07-25 16:30:20 -0700

    updates

--- modules/qtile.nix
+++ modules/qtile.nix
@@ -1,34 +1,32 @@
 { config, pkgs, ... }:
 
 {
-    # fonts = {
-    #     fonts = with pkgs; [
-    #         material-design-icons
-    #         noto-fonts
-    #         noto-fonts-cjk
-    #         noto-fonts-emoji
-    #         (nerdfonts.override { fonts = [ "DroidSansMono" "FiraCode" "Hack" "Iosevka" "JetBrainsMono"  "RobotoMono" ]; })
-    #     ];
-    #
-    #     enableDefaultFonts = false;
-    #
-    #     fontconfig.defaultFonts = {
-    #         serif = [ "Noto Serif" "Noto Color Emoji" ];
-    #         sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
-    #         monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
-    #         emoji = [ "Noto Color Emoji" ];
-    #     };
-    # };
+    fonts = {
+        fonts = with pkgs; [
+            material-design-icons
+            noto-fonts
+            noto-fonts-cjk
+            noto-fonts-emoji
+            (nerdfonts.override { fonts = [ "DroidSansMono" "FiraCode" "Hack" "Iosevka" "JetBrainsMono" "RobotoMono" ]; })
+        ];
+
+        enableDefaultFonts = false;
+
+        fontconfig.defaultFonts = {
+            serif = [ "Noto Serif" "Noto Color Emoji" ];
+            sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
+            monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+            emoji = [ "Noto Color Emoji" ];
+        };
+    };
 
     services.xserver.enable = true;
-    # services.xserver.displayManager.startx.enable = true;
-    # services.xserver.autorun = false;
-    services.xserver.displayManager.sddm.enable = true;
-    services.xserver.windowManager.i3.enable = true;
-    services.xserver = {
-        layout = "us";
-        xkbVariant = "";
-    };
+    services.xserver.displayManager.startx.enable = true;
+    services.xserver.windowManager.qtile.enable = true;
+    # services.xserver = {
+    #     layout = "us";
+    #     xkbVariant = "";
+    # };
 
     programs.dconf.enable = true;
 
