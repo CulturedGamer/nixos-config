4c9ca63e07b0ad9fbe2b5e690b67d6369a067830
CulturedGamer <dony357@outlook.com>
2023-07-31 12:37:24 -0700

    changes

--- flake.nix
+++ flake.nix
@@ -89,10 +89,6 @@
         };
     in
     {
-        overlays = [
-            (_: prev: { inherit (dwm.packages.${prev.system}) dwm; })
-        ];
-
         nixosConfigurations = 
         let 
             system = "x86_64-linux"; 
@@ -118,6 +114,10 @@
                     ./hosts/laptop
                 ] ++ activateSession.qtile;
             };
+
+            overlays = [
+                (final: prev: {dwm = inputs.dwm.packages.${system};})
+            ];
         };
     };
 }
