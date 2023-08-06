9d06928627f6b29cd2aa339c39fc21ab817c1631
CulturedGamer <dony357@outlook.com>
2023-08-06 14:31:18 -0700

    commit

--- modules/environments/dwm-environment/custom/alacritty.nix
+++ modules/environments/dwm-environment/custom/alacritty.nix
@@ -21,8 +21,8 @@
         enable = true;
         settings = {
             import = [
-                /home/donny/.config/alacritty/colors/rose-pine.yml
-                /home/donny/.config/alacritty/fonts/firacode.yml
+                "/home/donny/.config/alacritty/colors/rose-pine.yml"
+                "/home/donny/.config/alacritty/fonts/firacode.yml"
             ];
             env.TERM = "xterm-256color";
             shell.program = "fish";
