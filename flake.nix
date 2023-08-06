223e399fcd73a89772e581aaeb24a0f9091a4773
CulturedGamer <dony357@outlook.com>
2023-08-06 15:52:36 -0700

    commit

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
                 inherit system specialArgs;
                 modules = [
                     ./hosts/vm
-                ] ++ activateSession.qtile;
+                ] ++ activateSession.dwm;
             };
 
             potatopc = nixpkgs.lib.nixosSystem {
