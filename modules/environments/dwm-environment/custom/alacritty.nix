44c9a32b49781d92c16b817aa2717248719dcc28
CulturedGamer <dony357@outlook.com>
2023-08-06 14:30:19 -0700

    commit

--- modules/environments/dwm-environment/custom/alacritty.nix
+++ modules/environments/dwm-environment/custom/alacritty.nix
@@ -24,8 +24,8 @@
                 /home/donny/.config/alacritty/colors/rose-pine.yml
                 /home/donny/.config/alacritty/fonts/firacode.yml
             ];
-            env.TERM = xterm-256color;
-            shell.program = fish;
+            env.TERM = "xterm-256color";
+            shell.program = "fish";
             window.opacity = 0.885;
         };
     };
