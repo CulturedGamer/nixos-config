e8e72ba7088ec3499ae9d57ad778f41bfa66997e
CulturedGamer <dony357@outlook.com>
2023-08-24 11:09:43 -0700

    waybar and system updates

--- home/graphic/wm/waybar/default.nix
+++ home/graphic/wm/waybar/default.nix
@@ -8,17 +8,57 @@
             mainBar = {
                 layer = "top";
                 position = "top";
-                height = 20;
+                height = 30;
                 output = [ "eDP-1" ];
 
                 modules-left = [ "hyprland/workspaces" ];
                 modules-center = [ "hyprland/window" ];
-                modules-right = [ "pulseaudio" "upower" "clock" ];
+                modules-right = [ "tray" "network" "battery" "disk"  "memory" "backlight" "pulseaudio" "clock" ];
+
+                backlight = {
+                    format = "{icon} {percent}%";
+                    format-icons = [ "" "" ];
+                };
+
+                battery = {
+                    format = "{icon} ";
+                    format-alt = "{icon} {capacity}%";
+                    format-icons = [" " " " " " " " " "];
+                    states = {
+                        warning = 30;
+                        critical = 10;
+                    };
+                };
 
                 clock = {
-                    interval = 60;
                     format = " {:%I:%M}";
-                    max-length = 25;
+                    format-alt = " {:%m/%d/%Y}";
+                };
+
+                disk = {
+                    format = "󰨣 {percentage_free}%";
+                    on-click = "nemo";
+                };
+
+                memory = {
+                    format = "󰍹 {}%";
+                    on-click = "alacritty -e htop";
+                };
+
+                network = {
+                    format = "{icon}";
+                    format-alt = "{essid} ({signalStrength}%) {icon}";
+                    format-icons = [ "󰤯" "󰤟" "󰤢" "󰤢" "󰤨" ];
+                };
+
+                pulseaudio = {
+                    format = "{icon} {volume}%";
+                    format-bluetooth = "{icon}󰂯 {volume}%";
+                    format-muted = "󰝟";
+                    format-icons = {
+                        default = [ "" "" ];
+                    };
+                    on-click = "pavucontrol";
                 };
             };
         };
