e5a062c053cbea94431928815940b3ef31ccacaa
veryfastman <dony357@outlook.com>
2023-09-16 12:42:54 -0700

    modified keyd file

--- hosts/common/keyd.nix
+++ hosts/common/keyd.nix
@@ -6,34 +6,23 @@
   ];
 
   environment.etc."keyd/default.conf".text = ''
-      [ids]
+    [ids]
 
-      *
+    *
 
-      [main]
+    [main]
 
-      # Maps capslock to escape when pressed and control when held.
-      capslock = overload(control, esc)
+    # Maps capslock to escape when pressed and control when held.
+    capslock = overload(control, esc)
 
-      # Remaps the escape key to capslock
-      esc = capslock
-    '';
+    # Remaps the escape key to capslock
+    esc = capslock
+  '';
 
   systemd.services = {
     # https://github.com/NixOS/nixpkgs/issues/59603#issuecomment-1356844284
     NetworkManager-wait-online.enable = false;
 
-    keyd = {
-      enable = true;
-      description = "keyd key remapping daemon";
-      unitConfig = {
-        Requires = "local-fs.target";
-        After = "local-fs.target";
-      };
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.keyd}/bin/keyd";
-      };
-    };
+    keyd.enable = true;
   };
 }
