1ae6746a99963fbb60dbe9ffbe0f2c3a2e398e54
veryfastman <dony357@outlook.com>
2023-09-16 12:38:08 -0700

    removed a neovim keybinding

--- modules/terminal/neovim/keymaps.lua
+++ modules/terminal/neovim/keymaps.lua
@@ -4,7 +4,6 @@ local ui = require("harpoon.ui")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("i", "jk", "<ESC>", opts)
 vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
 vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
 vim.keymap.set("n", "<C-Right>", ":vertical resize -2<CR>", opts)
