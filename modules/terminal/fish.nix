5be9780100a9797088fca00ded01c92abfa32822
veryfastman <dony357@outlook.com>
2023-11-26 13:09:52 -0800

    added more aliases && nix flake update

--- modules/terminal/fish.nix
+++ modules/terminal/fish.nix
@@ -18,14 +18,19 @@ in
       shellAliases = mkIf (cfg.enableShellAliases) {
         v = "nvim";
         nd = "cd /etc/nixos";
+        desksw = "sudo nixos-rebuild switch --flake /etc/nixos#desktop";
+        deskte = "sudo nixos-rebuild test --flake /etc/nixos#desktop";
         diro = "ll | awk '(NR>1) { print $9 }'";
         fdh = "cd $(find . -type d -path '*/.*' -prune -o -not -name '.*' -type d -print | fzf)";
         fishrc = "sudoedit /etc/nixos/modules/terminal/fish.nix";
+        lapsw = "sudo nixos-rebuild switch --flake /etc/nixos#laptop";
+        lapte = "sudo nixos-rebuild test --flake /etc/nixos#laptop";
         lg = "lazygit";
         nluaconf = "cd /etc/nixos/modules/terminal/neovim";
         se = "sudoedit";
         snip = "grim -g '$(slurp)'";
         tms = "tmux new-session -c . -s";
+        trcl = "sudo nix-collect-garbage -d ; nix-collect-garbage -d";
         upd = "cd /etc/nixos && nix flake update";
         ytaudio = "yt-dlp --extract-audio --audio-format mp3";
         ytvideo = "yt-dlp -f 'bv*[height=1080]+ba'";
