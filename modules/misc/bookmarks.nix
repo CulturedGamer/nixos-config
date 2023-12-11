c30f1b5b5e596ec18d2cd0b93195a1aa74fec082
veryfastman <dony357@outlook.com>
2023-12-10 17:35:41 -0800

    added new bookmark

--- modules/misc/bookmarks.nix
+++ modules/misc/bookmarks.nix
@@ -8,7 +8,8 @@
     https://10fastfingers.com/typing-test/english
     https://github.com/veryfastman
     https://github.com/veryfastman/nixos-config
-    https://arewewaylandyet.com/
+    https://arewewaylandyet.com
     https://search.nixos.org/packages
+    https://noogle.dev
   '';
 }
